049dd688e5f28e4bd0990c5d7f9ef32aee62fd8e
So-fire <sophiaudofiah@gmail.com>
2023-07-14 11:26:57 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - code 4
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
