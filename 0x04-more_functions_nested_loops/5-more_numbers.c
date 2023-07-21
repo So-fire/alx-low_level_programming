ac22127a0fe3e55916afe857397dd6e38d238ee5
So-fire <sophiaudofiah@gmail.com>
2023-07-21 07:12:46 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * File: This is the file _isdigit.c
+ */
+
+/**
+ * more_numbers - Function to check the upper case character
+ * Return: 1 if the uppercase c else return 0.
+ *
+ */
+void more_numbers(void)
+{
+	int numbers, compute;
+
+	for (compute = 0; compute <= 9; compute++)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+		{
+			if (numbers > 9)
+				_putchar((numbers / 10) + '0');
+			_putchar((numbers % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
