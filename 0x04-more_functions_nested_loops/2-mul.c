96a835366911ea2113a8a50f2980e9512e3c4fa2
So-fire <sophiaudofiah@gmail.com>
2023-07-21 06:33:42 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * File: The file 2-mul.c
+ */
+
+/**
+ * mul - Function to check the upper case character
+ * Return: 1 if the uppercase c else return 0.
+ *
+ * @a: This is the first digit
+ * @b: This is the second digit
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
