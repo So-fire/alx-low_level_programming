0a8148eaccb0ebef70a94dd6df6008798c37d4ee
So-fire <sophiaudofiah@gmail.com>
2023-07-24 12:28:38 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int string_length = 0;
+while (s[string_length])
+string_length++;
+return (string_length);
+}
